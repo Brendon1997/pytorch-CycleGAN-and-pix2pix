ce0e95c24d17bbedcf16365cb992c812de013f54
brendonshuke <brendon@dev.al>
2024-11-13 10:58:13 -0500

    add dataset for summer2winter

---
+++ resize_photo.py
@@ -0,0 +1,30 @@
+import os
+from PIL import Image
+
+# Paths to the folders
+input_folder = '/Users/brendonshuke/Downloads/drive-download-20241113T151241Z-001'
+output_folder = '/Users/brendonshuke/Downloads/drive-download-20241113T151241Z-001/resized'
+
+# Make sure the output folder exists
+os.makedirs(output_folder, exist_ok=True)
+
+# Desired width and height
+new_width = 250
+new_height = 250
+
+# Process each image in the input folder
+for filename in os.listdir(input_folder):
+    if filename.endswith(('.jpg', '.jpeg', '.png')):  # Add more extensions if needed
+        img_path = os.path.join(input_folder, filename)
+
+        with Image.open(img_path) as img:
+            # Resize image while maintaining aspect ratio
+            # aspect_ratio = img.width / img.height
+            # adjusted_height = int(new_width / aspect_ratio)
+            resized_img = img.resize((new_width, new_height))
+
+            # Save resized image to the output folder
+            output_path = os.path.join(output_folder, filename)
+            resized_img.save(output_path)
+
+print("All images have been resized and saved to the output folder.")
